f42b7802165d1fef5405f0dc1e35c63fb4d9292a
ALLEN <aljay3334@gmail.com>
2022-08-18 19:11:21 +0000

    Added UserModel containing its constructor and methods

--- src/app.py
+++ src/app.py
@@ -19,6 +19,6 @@ def create_app(env_name):
     """
     example endpoint
     """
-    return 'Congratulations! Your first endpoint is workin'
+    return 'Congratulations! Your first endpoint is working'
 
   return app

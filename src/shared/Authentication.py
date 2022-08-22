769891a87a4bfbc7a6bcf12d9d49bd999fae27ef
ALLEN <aljay3334@gmail.com>
2022-08-22 22:16:45 +0000

    Added auth decorator method in the Auth class for protected routes
    If user is not logged in, he can't access certain routes

--- src/shared/Authentication.py
+++ src/shared/Authentication.py
@@ -2,7 +2,7 @@
 import jwt
 import os
 import datetime
-from flask import json,Response
+from flask import json,Response,request,g
 from ..models.UserModel import UserModel
 from dotenv import load_dotenv
 load_dotenv()
@@ -35,6 +35,9 @@ class Auth():
         status=400
       )
 
+
+
+  #decorator
   @staticmethod
   def decode_token(token):
     """
@@ -51,3 +54,38 @@ class Auth():
     except jwt.InvalidTokenError:
       re['error'] = {'message': 'Invalid token, please try again with a new token'}
       return re
+
+   # decorator
+  @staticmethod
+  def auth_required(func):
+    """
+    Auth decorator
+    """
+    @wraps(func)
+    def decorated_auth(*args, **kwargs):
+      if 'api-token' not in request.headers:
+        return Response(
+          mimetype="application/json",
+          response=json.dumps({'error': 'Authentication token is not available, please login to get one'}),
+          status=400
+        )
+      token = request.headers.get('api-token')
+      data = Auth.decode_token(token)
+      if data['error']:
+        return Response(
+          mimetype="application/json",
+          response=json.dumps(data['error']),
+          status=400
+        )
+        
+      user_id = data['data']['user_id']
+      check_user = UserModel.get_one_user(user_id)
+      if not check_user:
+        return Response(
+          mimetype="application/json",
+          response=json.dumps({'error': 'user does not exist, invalid token'}),
+          status=400
+        )
+      g.user = {'id': user_id}
+      return func(*args, **kwargs)
+    return decorated_auth

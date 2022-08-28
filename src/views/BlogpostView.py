3e2efb64a81f70b80df17aabf3e1d811551b31ec
ALLEN <aljay3334@gmail.com>
2022-08-28 21:45:22 +0000

    Added a route for getting a single post with an id

--- src/views/BlogpostView.py
+++ src/views/BlogpostView.py
@@ -38,6 +38,21 @@ def get_all():
   data = blogpost_schema.dump(posts, many=True)
   return custom_response(data, 200)
 
+
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['GET'])
+def get_one(blogpost_id):
+  """
+  Get A Blogpost
+  """
+  post = BlogpostModel.get_one_blogpost(blogpost_id)
+  if not post:
+    return custom_response({'error': 'post not found'}, 404)
+  data = blogpost_schema.dump(post)
+  return custom_response(data, 200)
+
+
+
 def custom_response(res, status_code):
   """
   Custom Response Function

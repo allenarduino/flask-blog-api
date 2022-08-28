2e5dfbb6ad1d6135205061452a4fe4904fb0b2d9
ALLEN <aljay3334@gmail.com>
2022-08-28 01:36:56 +0000

    Added a route  for getting a single user

--- src/views/UserView.py
+++ src/views/UserView.py
@@ -79,6 +79,19 @@ def get_all():
   ser_users = user_schema.dump(users, many=True)
   return custom_response(ser_users, 200)
 
+@user_api.route('/<int:user_id>', methods=['GET'])
+@Auth.auth_required
+def get_a_user(user_id):
+  """
+  Get a single user
+  """
+  user = UserModel.get_one_user(user_id)
+  if not user:
+    return custom_response({'error': 'user not found'}, 404)
+  
+  ser_user = user_schema.dump(user)
+  return custom_response(ser_user, 200)
+
 
   
 
